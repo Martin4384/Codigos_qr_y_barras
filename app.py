36dc6a678f230d2711bba1d7ce6f99f41d160e8c
Martin4384 <martinramirezguzman6@mail.com>
2024-11-06 12:47:57 -0600

    cuarto cambio

--- app.py
+++ app.py
@@ -10,23 +10,39 @@ app = Flask(__name__)
 @app.route('/qr_code', methods=['GET'])
 def generate_qr():
     data = request.args.get('data')
-    qr_img = qrcode.make(data)
+    
+    # Configurar el tamaño del QR
+    qr = qrcode.QRCode(
+        version=1,  # Controla el tamaño del QR (puedes aumentar si necesitas más datos)
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=5,  # Tamaño de cada cuadro del QR (reduce para hacer el QR más pequeño)
+        border=2    # Borde alrededor del QR (reduce para un borde más pequeño)
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+    
+    qr_img = qr.make_image(fill="black", back_color="white")
     buffer = BytesIO()
     qr_img.save(buffer, format="PNG")
     buffer.seek(0)
     return send_file(buffer, mimetype='image/png')
-
 # Ruta para generar código de barras
 @app.route('/barcode', methods=['GET'])
 def generate_barcode():
     data = request.args.get('data')
-     # Verificar si data está vacío o no válido
     if not data:
         return "Error: El parámetro 'data' está vacío o no es válido", 400
-
-    barcode_img = barcode.get('code128', data, writer=ImageWriter())
+    
+    barcode_img = Code128(data, writer=ImageWriter())
+    options = {
+        'module_width': 0.2,  # Ajusta el ancho del módulo (reduce para imágenes más pequeñas)
+        'module_height': 10.0,  # Ajusta la altura del módulo
+        'font_size': 10,       # Tamaño de la fuente
+        'text_distance': 1.0   # Distancia del texto al código de barras
+    }
+    
     buffer = BytesIO()
-    barcode_img.write(buffer)
+    barcode_img.write(buffer, options)
     buffer.seek(0)
     return send_file(buffer, mimetype='image/png')
 
